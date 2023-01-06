a3db0cf1948d708309eb8ab9e81e326d99bf6c15
Porter <porterhatch@Porters-MacBook-Pro-5.local>
2023-01-06 12:59:21 -0700

    finished grade program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,47 @@
+// Porter Hatch 
+// Jan 6 2022
+// Practice
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string ph_grade = Console.ReadLine();
+        int ph_grade_percentage = int.Parse(ph_grade);
+        
+        string ph_letter = "";
+
+        if (ph_grade_percentage >= 90)
+        {
+            ph_letter = "A";
+        }
+        else if (ph_grade_percentage >= 80)
+        {
+            ph_letter = "B";
+        }
+        else if (ph_grade_percentage >= 70)
+        {
+            ph_letter = "C";
+        }
+        else if (ph_grade_percentage >= 60)
+        {
+            ph_letter = "D";
+        }
+        else 
+        {
+            ph_letter = "F";
+        }
+        Console.WriteLine($"Your grade is: {ph_letter}");
+
+        if (ph_grade_percentage >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
