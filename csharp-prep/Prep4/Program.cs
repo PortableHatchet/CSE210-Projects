9476049808f863ace06f468aead7154e74364968
Porter Hatch <porterhatch1@gmail.com>
2023-01-19 11:42:27 -0700

    added comments to the project

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,10 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Create a list for the userNumbers to go into 
         List<int> numbersList = new List<int>();
         int userNumber = -1;
         while (userNumber != 0)
         {
+            // Add numbers to list
             Console.Write("Enter a list of numbers. Type 0 when finished. ");
             userNumber = int.Parse(Console.ReadLine());
             numbersList.Add(userNumber);
@@ -16,14 +18,17 @@ class Program
         {
             Console.WriteLine(item);
         }
+        // Sum the numbers
         int sum = 0;
         sum = numbersList.Sum();
         Console.WriteLine($"The sum is: {sum}");
 
+        // Average the numbers in the list
         float average = 0;
         average = ((float)sum) / numbersList.Count;
         Console.WriteLine($"The average is: {average}");
 
+        // Find largest number in the list
         int largestNum = numbersList[0];
         foreach (int number in numbersList)
         {
